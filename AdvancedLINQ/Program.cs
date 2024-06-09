34d01be53473871bb25a56217493234cb20fea09
Victor Willian <victorwillian1012@gmail.com>
2024-06-09 10:47:23 -0300

    chore: :white_check_mark: More LINQ functions
    Learn about Orderby, Take, Skip, Select, ToList and inline ForEach

--- AdvancedLINQ/Program.cs
+++ AdvancedLINQ/Program.cs
@@ -2,14 +2,38 @@
 using System.Text.Json.Serialization;
 
 var fileContent = await File.ReadAllTextAsync("./MOCK_DATA.json");
-var cars = JsonSerializer.Deserialize<IEnumerable<CarData>>(fileContent);
+// The ! ensure that car is not null
+IEnumerable<CarData> cars = JsonSerializer.Deserialize<IEnumerable<CarData>>(fileContent)!;
 
-var carsWithAtLeastFourDoors = cars!.Where(c => c.NumberOfDoors >= 4);
+var carsWithAtLeastFourDoors = cars.Where(c => c.NumberOfDoors >= 4);
 
-foreach (var car in carsWithAtLeastFourDoors)
-{
-  Console.WriteLine($"The car {car.Model} has {car.NumberOfDoors} doors!");
-}
+// foreach (var car in carsWithAtLeastFourDoors)
+// {
+//   Console.WriteLine($"The car {car.Model} has {car.NumberOfDoors} doors!");
+// }
+
+var mazdasWithAtLeastFourDoors = cars
+                                  .Where(c => c.Make == "Mazda")
+                                  .Where(c => c.NumberOfDoors >= 4);
+
+// foreach (var car in mazdasWithAtLeastFourDoors)
+// {
+//   Console.WriteLine($"The Mazdar car {car.Model} has {car.NumberOfDoors} doors!");
+// }
+
+// cars
+//   .Where(c => c.Make.StartsWith('M'))
+//   .Select(c => $"{c.Make} - {c.Model}")
+//   .ToList()
+//   .ForEach(c => Console.WriteLine($"Start with M: {c}"));
+
+// Display a list of the 10 most powerfull cars (in term of hp)
+cars
+  .OrderByDescending(c => c.HP)
+  .Take(10) // Skip(page * items_per_page) and Take(items_per_page) to paginations
+  .Select(c => $"{c.Make} HP: {c.HP}")
+  .ToList()
+  .ForEach(Console.WriteLine);
 
 class CarData
 {
