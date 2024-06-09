e8b988903158c9b1805020a8d1f9ceae89c08d8e
Victor Willian <victorwillian1012@gmail.com>
2024-06-09 17:05:44 -0300

    chore: :white_check_mark: Learn about EntityFramework

---
+++ LearnEF/Migrations/20240609200347_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LearnEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Notes = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Stars = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    UniOfMeasure = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    DishId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_DishId",
+                table: "Ingredients",
+                column: "DishId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+        }
+    }
+}
---
+++ LearnEF/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+// Create the factory
+var factory = new CookBookContextFactory();
+
+// Create the context
+using var context = factory.CreateDbContext(args);
+
+var newDish = new Dish { Title = "Foo", Notes = "Bar" };
+context.Dishes.Add(newDish);
+await context.SaveChangesAsync();
+
+newDish.Notes = "Baz";
+await context.SaveChangesAsync();
+
+await EntityStates(factory);
+await ChangeTracking(factory);
+
+static async Task EntityStates(CookBookContextFactory dbFactory)
+{
+  using var dbContext = dbFactory.CreateDbContext();
+  var newDish = new Dish { Title = "Foo", Notes = "Bar" };
+
+  var state = dbContext.Entry(newDish).State; // detached
+
+  dbContext.Dishes.Add(newDish);
+  state = dbContext.Entry(newDish).State; // added
+
+  await dbContext.SaveChangesAsync();
+  state = dbContext.Entry(newDish).State; // unchanged
+
+  newDish.Notes = "Other Bar";
+  state = dbContext.Entry(newDish).State; // modified
+
+  await dbContext.SaveChangesAsync();
+  state = dbContext.Entry(newDish).State; // unchanged
+
+  dbContext.Remove(newDish);
+  state = dbContext.Entry(newDish).State; // deleted
+
+  await dbContext.SaveChangesAsync();
+  state = dbContext.Entry(newDish).State; // detached
+}
+
+static async Task ChangeTracking(CookBookContextFactory dbFactory)
+{
+  using var dbContext = dbFactory.CreateDbContext();
+
+  var newDish = new Dish { Title = "Foo", Notes = "Bar" };
+  dbContext.Dishes.Add(newDish);
+  await dbContext.SaveChangesAsync();
+
+  newDish.Notes = "Bar";
+  var entry = dbContext.Entry(newDish);
+  var originalValue = entry.OriginalValues[nameof(Dish.Notes)]!.ToString();
+}
+
+#region Models
+class Dish
+{
+  public int Id { get; set; }
+
+  [MaxLength(100)]
+  public string Title { get; set; } = string.Empty;
+
+  [MaxLength(1000)]
+  public string? Notes { get; set; }
+
+  public int? Stars { get; set; }
+
+  public List<DishIngredients>? Ingredients { get; set; } = new();
+}
+
+class DishIngredients
+{
+  public int Id { get; set; }
+
+  [MaxLength(100)]
+  public string Description { get; set; } = string.Empty;
+
+  [MaxLength(50)]
+  public string UniOfMeasure { get; set; } = string.Empty;
+
+  [Column(TypeName = "decimal(5,2)")]
+  public decimal Amount { get; set; }
+
+  public Dish? Dish { get; set; }
+
+  public int DishId { get; set; }
+}
+
+#endregion
+
+#region Context
+class CookBookContext : DbContext
+{
+  public DbSet<Dish> Dishes { get; set; }
+
+  public DbSet<DishIngredients> Ingredients { get; set; }
+
+  public CookBookContext(DbContextOptions<CookBookContext> options)
+      : base(options) { }
+}
+
+#endregion
+
+#region Factory
+class CookBookContextFactory : IDesignTimeDbContextFactory<CookBookContext>
+{
+  public CookBookContext CreateDbContext(string[]? args = null)
+  {
+    var builder = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", false, true)
+        .Build();
+
+    var optionsBuilder = new DbContextOptionsBuilder<CookBookContext>();
+    // postgresql
+    optionsBuilder.UseNpgsql(builder.GetConnectionString("DefaultConnection"));
+
+    return new CookBookContext(optionsBuilder.Options);
+  }
+}
+#endregion
